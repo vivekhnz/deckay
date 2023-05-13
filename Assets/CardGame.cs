f1b6c3ea14be4f70977d89cb38289dfdffb84480
steerocea <109568273+steerocea@users.noreply.github.com>
2023-05-13 20:18:03 +1200

    added file

---
+++ Assets/CardGame.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Hand
+{
+    Player,
+    Opponent
+}
+
+internal class CardGame
+{
+    private Deck deck;
+
+    public List<CardData> PlayerHand;
+    public List<CardData> OpponentHand;
+
+    public CardGame()
+	{
+		deck = new Deck();
+        PlayerHand = new List<CardData>();
+        OpponentHand = new List<CardData>();
+	}
+
+    internal void DealCards(int cardsPerHand)
+    {
+        PlayerHand.Clear();
+        for (int i = 0; i < cardsPerHand; i++)
+        {
+            var cardData = deck.GetNextCard();
+            PlayerHand.Add(cardData);
+        }
+
+        OpponentHand.Clear();
+        for (int i = 0; i < cardsPerHand; i++)
+        {
+            var cardData = deck.GetNextCard();
+            cardData.IsFaceDown = true;
+            OpponentHand.Add(cardData);
+        }
+    }
+
+    internal CardData PickUpCard(Hand hand)
+    {
+        var card = deck.GetNextCard();
+
+        switch (hand)
+        {
+            case Hand.Player:
+                PlayerHand.Add(card);
+                break;
+            case Hand.Opponent:
+                card.IsFaceDown = true;
+                OpponentHand.Add(card);
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+
+        return card;
+    }
+}
